04ad948c90080eb663e85f63a5556406741a7206
No Hannah <67176677+nohannah@users.noreply.github.com>
2022-10-06 17:39:14 +0700

    staus and admin already exist

--- add-admin.php
+++ add-admin.php
@@ -3,6 +3,14 @@
     <div class="wrapper">
         <h1> Add Admin</h1>
         <br><br>
+        <?php 
+            if(isset($_SESSION['Exist']))
+                {
+                    echo $_SESSION['Exist'];//Displaying Session Message
+                    unset ($_SESSION['Exist']);//Removing Session Message 
+                }
+            
+        ?>
         <form action="" method="POST">
         <table class="tbl-30">
             <tr>
@@ -21,12 +29,20 @@
                     <input type="password" name="password" placeholder="Your Password">
                 </td>
             </tr>
-
+            <tr>
+                <td>Status:</td>
+                <td>
+                    <input type="radio" name="active" value="yes">Active
+                    <input type="radio" name="inactive" value="no">Inactive
+                </td>
+            </tr>
             <tr>
                 <td colspan="2">
                     <input type="submit" name="submit" value="Add Admin" class="btn-secondary">
                 </td>
             </tr>
+
+           
         </table>
         </form>
         
@@ -49,15 +65,38 @@
          $username  = $_POST['user_name'];
          $password  = md5($_POST['password']); //Password Encription with MD5
 
+
+        if(isset($_POST['active']))
+        {
+            $status=1;
+        }
+        else{
+            $status=0;
+        }
+        //Select usename colum which show exist
+         $select =mysqli_query($conn,"SELECT*FROM tbl_admin WHERE username='$username'");
+         $count=mysqli_num_rows($select);
+         if($count==1)
+          {
+
+            $_SESSION['Exist'] = "<div class='success'>User name already Exist.</div>";
+            //Redirect Page To Manage Admin
+            header("location:".SITEURL.'admin/add-admin.php');
+            die();
+         }
+         
+        
          //SQL Queury to save the data into database
          $slq = "INSERT INTO tbl_admin SET 
                 full_name = '$full_name',
                 username  = '$username',
-                password  = '$password'
+                password  = '$password',
+                status=$status
+
          ";
-        //3. Executing Query and Saving Data into Database
+         //3. Executing Query and Saving Data into Database
          $res = mysqli_query($conn,$slq) or die(mysqli_error());
-         //4. Check whether the (Query is Executed) data is inserted or not and display appropriate massege
+         
          if($res==TRUE)
          {
             //Data Inserted
@@ -65,14 +104,41 @@
             //Create a Session Variable to display message
             $_SESSION['add'] = "<div class='success'>Admin Added Successfully.</div>";
             //Redirect Page To Manage Admin
-            header("location:".SITEURL.'admin/admin1.php');
+            header("location:".SITEURL.'admin/admin.php');
          }else{
             //Failed to Insert Data
             //echo "Faile to Inserte Data";
                 //Create a Session Variable to display message
                 $_SESSION['add'] = "<div class='error'>Failed to Add Admin</div>";
                 //Redirect Page To Add Admin
-                header("location:".SITEURL.'admin/add-admin1.php');
+                header("location:".SITEURL.'admin/add-admin.php');
          }
+         
+        /*    $select =mysqli_query($conn,"SELECT*FROM tbl_admin WHERE username='".$_POST['username']."'");
+            if(mysqli_num_rows($select))
+             {
+                exit('This username already exists');
+            }
+         /*
+        //3. Executing Query and Saving Data into Database
+         $res = mysqli_query($conn,$slq) or die(mysqli_error());
+         
+         if($res==TRUE)
+         {
+            //Data Inserted
+            //echo "Data Inserted";
+            //Create a Session Variable to display message
+            $_SESSION['add'] = "<div class='success'>Admin Added Successfully.</div>";
+            //Redirect Page To Manage Admin
+            header("location:".SITEURL.'admin/admin.php');
+         }else{
+            //Failed to Insert Data
+            //echo "Faile to Inserte Data";
+                //Create a Session Variable to display message
+                $_SESSION['add'] = "<div class='error'>Failed to Add Admin</div>";
+                //Redirect Page To Add Admin
+                header("location:".SITEURL.'admin/add-admin.php');
+         }
+         */
     }
 ?>
