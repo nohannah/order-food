4212fbb2c23843e1d98417806725d0cf12b15f91
No Hannah <67176677+nohannah@users.noreply.github.com>
2022-10-11 17:27:58 +0700

    Partial in Admin back-end code

--- menu.php
+++ menu.php
@@ -1,22 +1,24 @@
 <?php include ("../config/constants.php")?>
+<?php include ("../admin/login-check.php") ?>
 <html>
     <head>
         <title>Order food</title>
+        <img src="partials/logo.png">
         <link rel="stylesheet" href="../css/index.css"> 
         
     </head>
-    <body>
+    <body class="body">
         <!-- Menu Section Starts -->
-    <img src=logo.png  class="logo">
+   
         <div>
             <div class=" menu  text-center ">       
         <ul>
             <li><a href="index.php">Home</a></li>
             <li><a href="admin.php">Admin</a></li>
-             <li><a href="#">Categories</a></li>
-            <li><a href="#">Food</a></li>
-            <li><a href="#">Contact</a></li>
-            <li><a href="#">Logout</a></li>
+             <li><a href="manage-categories.php">Categories</a></li>
+            <li><a href="food.php">Food</a></li>
+            <li><a href="Contact.php">Contact</a></li>
+            <li><a href="logout.php">Logout</a></li>
         </ul>
 </div>
 </div>
