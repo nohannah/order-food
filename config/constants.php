85c686fe664db223ca861343011a23e14771d7f4
No Hannah <67176677+nohannah@users.noreply.github.com>
2022-11-15 14:37:43 +0700

    config

---
+++ config/constants.php
@@ -0,0 +1,15 @@
+
+<?php 
+    //Start Session
+    session_start(); 
+    //Create Constants to Store Non Repeating Values
+    define('SITEURL','http://localhost/food order/');
+    //define('SITEURL','http://localhost/food-order/');
+    define('LOCALHOST','localhost');
+    define('DB_USERNAME','root');
+    define('DB_PASSWORD','');
+    define('DB_NAME','food_orderdb');
+    date_default_timezone_set('Asia/Phnom_Penh');  
+    $conn = mysqli_connect(LOCALHOST,DB_USERNAME,DB_PASSWORD) or die(mysqli_error()); //Database Connection
+    $db_Select = mysqli_select_db($conn,DB_NAME)or die(mysqli_error()); //Select Database
+?>
